112b3cced9ff9564839b57f2964f51e6c06227a4
maajidmuhammad <abdimohamed0742@gmail.com>
2024-04-06 22:14:10 +0300

    completed assignments

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+    };
+
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(formData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = data.id;
+        document.body.append(`New ID: ${newId}`);
+    })
+    .catch(error => {
+        document.body.append(`Error: ${error.message}`);
+    });
+}
